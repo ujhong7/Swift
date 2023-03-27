3184034301ff7297e6d59e88a997886d3a5f14f8
ujhong7 <ujhong7@gmail.com>
2023-03-27 11:37:04 +0900

    비동기

--- concurrency.playground/Contents.swift
+++ concurrency.playground/Contents.swift
@@ -218,26 +218,319 @@ let privateQueue = DispatchQueue(label: "com.inflearn.serial")
 
 // UI업데이트는 메인 쓰레드에서
 // 유저인터페이스(즉, 화면)와 관련된 작업은 메인쓰레드에서 진행해야 함
+var imageView: UIImageView? = nil
+
+let url = URL(string: "https://bit.ly/32ps0DI")!
+
+// URL세션은 내부적으로 비동기로 처리된 함수임.
+URLSession.shared.dataTask(with: url) { (data, response, error) in
+    
+    if error != nil{
+        print("에러있음")
+    }
+    
+    guard let imageData = data else { return }
+    
+    // 즉, 데이터를 가지고 이미지로 변형하는 코드
+    let photoImage = UIImage(data: imageData)
+    
+    // 🎾 이미지 표시는 DispatchQueue.main에서 🎾
+    DispatchQueue.main.async {
+        imageView?.image = photoImage
+    }
+    
+    
+}.resume()
+
+// UI와 관련된 일은 다시 메인쓰레드로 보내야 함
+DispatchQueue.global().async {
+    
+    // 비동기적인 작업들 ===> 네트워크 통신 (데이터 다운로드)
+    
+    DispatchQueue.main.async {
+        // UI와 관련된 작업은
+    }
+}
+
+
+// ========================================================================
+// ========================================================================
+
+
+// 올바른 비동기함수의 설계
+// 리턴(return)이 아닌 콜백함수를 통해, 끝나는 시점을 알려줘야 한다.
+
+// 함수(메서드)를 아래처럼 설계하면 절대 안된다.
+func getImages(with urlString: String) -> UIImage? {
+    
+    let url = URL(string: urlString)!
+    
+    var photoImage: UIImage? = nil
+    
+    URLSession.shared.dataTask(with: url) { (data, response, error) in
+        if error != nil {
+            print("에러있음: \(error!)")
+        }
+        // 옵셔널 바인딩
+        guard let imageData = data else { return }
+        
+        // 데이터를 UIImage 타입으로 변형
+        photoImage = UIImage(data: imageData)
+        
+    }.resume()
+
+    
+    return photoImage    // 항상 nil 이 나옴
+}
+
+getImages(with: "https://bit.ly/32ps0DI")    // 무조건 nil로 리턴함 ⭐️
 
 
 
+// 올바른 함수(메서드)의 설계 - 콜백함수의 사용법
+func properlyGetImages(with urlString: String, completionHandler: @escaping (UIImage?) -> Void) {
+    
+    let url = URL(string: urlString)!
+    
+    var photoImage: UIImage? = nil
+    
+    URLSession.shared.dataTask(with: url) { (data, response, error) in
+        if error != nil {
+            print("에러있음: \(error!)")
+        }
+        // 옵셔널 바인딩
+        guard let imageData = data else { return }
+        
+        // 데이터를 UIImage 타입으로 변형
+        photoImage = UIImage(data: imageData)
+        
+        completionHandler(photoImage)
+        
+    }.resume()
+    
+}
+
+// 올바르게 설계한 함수 실행
+properlyGetImages(with: "https://bit.ly/32ps0DI") { (image) in
+    
+    // 처리 관련 코드 넣는 곳...
+    
+    DispatchQueue.main.async {
+        // UI관련작업의 처리는 여기서
+    }
+    
+}
+
+
+// ========================================================================
+// ========================================================================
+
+
+// 클로저의 강한 참조 주의!
+// 강한참조가 일어나고, (서로가 서로를 가리키는) 강한 참조 사이클은 일어나지 않지만
+// 생각해볼 부분이 있다
+class ViewController: UIViewController {
+    
+    var name: String = "뷰컨"
+    
+    func doSomething() {
+        DispatchQueue.global().async {
+            sleep(3)
+            print("글로벌큐에서 출력하기: \(self.name)")
+        }
+    }
+    
+    deinit {
+        print("\(name) 메모리 해제")
+    }
+}
+
+func localScopeFunction() {
+    let vc = ViewController()
+    vc.doSomething()
+}
+
+localScopeFunction()
+//글로벌큐에서 출력하기: 뷰컨
+
+//뷰컨 메모리 해제
+/**=======================================================
+ - (글로벌큐)클로저가 강하게 캡처하기 때문에, 뷰컨트롤러의 RC가 유지되어
+ - 뷰컨트롤러가 해제되었음에도, 3초뒤에 출력하고 난 후 해제됨
+ - (강한 순환 참조가 일어나진 않지만, 뷰컨트롤러가 필요없음에도 오래 머무름)
+
+ - 그리고 뷰컨트롤러가 사라졌음에도, 출력하는 일을 계속함
+=========================================================**/
 
 
 
+// 약한참조
+class ViewController1: UIViewController {
+    
+    var name: String = "뷰컨"
+    
+    func doSomething() {
+        // 강한 참조 사이클이 일어나지 않지만, 굳이 뷰컨트롤러를 길게 잡아둘 필요가 없다면
+        // weak self로 선언
+        DispatchQueue.global().async { [weak self] in
+            guard let weakSelf = self else { return }
+            sleep(3)
+            print("글로벌큐에서 출력하기: \(weakSelf.name)")
+        }
+    }
+    
+    deinit {
+        print("\(name) 메모리 해제")
+    }
+}
+
+func localScopeFunction1() {
+    let vc = ViewController1()
+    vc.doSomething()
+}
+
+localScopeFunction1()
+//뷰컨 메모리 해제
+
+//글로벌큐에서 출력하기: nil
+/**=======================================================
+ - 뷰컨트롤러를 오래동안 잡아두지 않음
+ - 뷰컨트롤러가 사라지면 ===> 출력하는 일을 계속하지 않도록 할 수 있음
+   (if let 바인딩 또는 guard let 바인딩까지 더해서 return 가능하도록)
+=========================================================**/
 
 
 
+// ========================================================================
+// ========================================================================
+
+
+// 작업을 오랫동안 실행하는 함수가 있다고 가정
+func longtimePrint(name: String) -> String {
+    print("프린트 - 1")
+    sleep(1)
+    print("프린트 - 2")
+    sleep(1)
+    print("프린트 - 3 이름:\(name)")
+    sleep(1)
+    print("프린트 - 4")
+    sleep(1)
+    print("프린트 - 5")
+    return "작업 종료"
+}
+
+longtimePrint(name: "잡스")
+// 프린트 - 1
+// 프린트 - 2
+// 프린트 - 3 이름:잡스
+// 프린트 - 4
+// 프린트 - 5
+
+
+// 동기함수를 비동기함수로 만들기!
+// 작업을 오랫동안 실행하는데, 동기적으로 동작하는 함수를
+// 비동기적으로 동작하도록 만들어, 반복적으로 사용하도록 만들기
+// 내부적으로 다른 큐로 비동기적으로 보내서 처리
+func asyncLongtimePrint(name: String, completion: @escaping (String) -> Void) {
+    DispatchQueue.global().async {
+        let n = longtimePrint(name: name)
+        completion(n)
+    }
+}
+
+//asyncLongtimePrint(name: "잡스", completion: <#T##(String) -> Void#>)
+
+asyncLongtimePrint(name: "잡스") { (result) in
+    print(result)
+    
+    // 메인쓰레드에서 처리해야하는 일이라면,
+//    DispatchQueue.main.async {
+//        print(result)
+//    }
+}
 
 
 
+// ========================================================================
+// ========================================================================
+
+
+// URLSession은 비동기 메서드!
+// API중에는 내부적으로 비동기처리가 된 메서드들이 존재
+let movieURL = "https://bit.ly/2QF3ID2"
+
+// 1. URL 구조체 만들기
+let url = URL(string: movieURL)!
+
+// 2. URLSession 만들기 (네트워킹을 하는 객체 - 브라우저 같은 역할)
+let session = URLSession.shared
+
+// 3. 세션에 (일시정지 상태로)작업 부여
+let task = session.dataTask(with: url) { (data, response, error) in
+    if error != nil {
+        print(error!)
+        return
+    }
+
+    guard let safeData = data else {
+        return
+    }
+
+    print(String(decoding: safeData, as: UTF8.self))
+
+}
+
+// 4.작업시작
+task.resume()   // 일시정지된 상태로 작업이 시작하기 때문
+
+// URLSession 사용하기! ➡︎ 내부적으로 비동기 동작한다는 것을 알고 써야함
+print("출력 - 1")
+
+URLSession.shared.dataTask(with: url) { (data, response, error) in
+    if error != nil {
+        print(error!)
+        return
+    }
+    
+    guard let safeData = data else {
+        return
+    }
+    
+    print(String(decoding: safeData, as: UTF8.self))
+    
+}.resume()
+
+print("출력 - 2")
+
+
+// 출력 - 1
+// 출력 - 2
+// ... data
 
 
 
+// ========================================================================
+// ========================================================================
 
 
+// 동시큐에서 직렬큐로 보내기
+// Thread-safe하지 않을때, 처리하는 방법
 
+// 배열은 여러쓰레드에서 동시에 접근하면 문제가 생길 수 있다.
+var array = [String]()
 
+let serialQueue = DispatchQueue(label: "serial")
 
+for i in 1...20 {
+    DispatchQueue.global().async {
+        print("\(i)")
+        //array.append("\(i)")    //  <===== 동시큐에서 실행하면 동시다발적으로 배열의 메모리에 접근
+        
+        serialQueue.async {        // 올바른 처리 ⭐️
+            array.append("\(i)")
+        }
+    }
+}
 
 
 
